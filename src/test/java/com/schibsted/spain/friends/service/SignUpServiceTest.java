2a98beec95fb5cff214b7304c9b8b94b8991d20b
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-07 13:25:08 +0100

    Deleted imports

--- src/test/java/com/schibsted/spain/friends/service/SignUpServiceTest.java
+++ src/test/java/com/schibsted/spain/friends/service/SignUpServiceTest.java
@@ -9,9 +9,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SignUpServiceTest {
