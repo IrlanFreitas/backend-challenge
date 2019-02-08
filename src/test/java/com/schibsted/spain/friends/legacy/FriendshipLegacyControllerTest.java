c666e423b44b239960eda2dfc1c8c51dc45e9975
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-08 09:55:23 +0100

    Added test to verify empty lists

--- src/test/java/com/schibsted/spain/friends/legacy/FriendshipLegacyControllerTest.java
+++ src/test/java/com/schibsted/spain/friends/legacy/FriendshipLegacyControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -88,6 +89,22 @@ public class FriendshipLegacyControllerTest {
 		FriendshipLegacyController friendshipLegacyController = new FriendshipLegacyController(friendShipService);
 
 		final List<String> expected = asList("Pepito", "Juanito");
+
+		when(friendShipService.list(userValid, password)).thenReturn(expected);
+
+		final Object actual = friendshipLegacyController.listFriends(userValid.getName(), "123456789ab");
+
+		assertEquals(expected, actual);
+
+		verify(friendShipService, times(1))
+				.list(userValid, password);
+	}
+
+	@Test
+	public void expectedEmptyWhenUserHasNoFriends() {
+		FriendshipLegacyController friendshipLegacyController = new FriendshipLegacyController(friendShipService);
+
+		final List<String> expected = emptyList();
 
 		when(friendShipService.list(userValid, password)).thenReturn(expected);
 
