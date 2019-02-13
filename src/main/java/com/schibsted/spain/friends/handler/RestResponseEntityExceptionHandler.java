d4fb809239b555dbc09756939a1e4fef847329cb
dsanchezdios <daniel.schezdios@gmail.com>
2019-02-14 00:33:44 +0100

    Removed comments

--- src/main/java/com/schibsted/spain/friends/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/schibsted/spain/friends/handler/RestResponseEntityExceptionHandler.java
@@ -11,11 +11,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-	// ya fue aceptada un 409
-	// requests de estado
-
-	// Crear una excepcion diferente para not founds
-	// Crear otra excepcion no uses illegal argument
 	@ExceptionHandler(value = {IllegalArgumentException.class})
 	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
 		return handleExceptionInternal(
